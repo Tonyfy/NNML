239ed065a61dcae187fd7a93659dccf54fae4bce
Tonyfy <1207675909@qq.com>
2016-12-27 21:20:16 +0800

    add a4 solution and lec14,15 ppt

--- lecture13/a4/cd1.m
+++ lecture13/a4/cd1.m
@@ -2,5 +2,27 @@ function ret = cd1(rbm_w, visible_data)
 % <rbm_w> is a matrix of size <number of hidden units> by <number of visible units>
 % <visible_data> is a (possibly but not necessarily binary) matrix of size <number of visible units> by <number of data cases>
 % The returned value is the gradient approximation produced by CD-1. It's of the same shape as <rbm_w>.
-    error('not yet implemented');
+
+visible_data = sample_bernoulli(visible_data);
+
+% 1 obtain hidden state
+hidden_probability = visible_state_to_hidden_probabilities(rbm_w, visible_data);
+%rbm_w = rbm_w + d_G_by_rbm_w;
+% 2 sample from hidden probability
+hidden_state = sample_bernoulli(hidden_probability);  %convert probability to binary state(>0.5)
+d_G_by_rbm_w0 = configuration_goodness_gradient(visible_data, hidden_state);
+
+% 3 reconstruct visible from hidden
+visible_probability = hidden_state_to_visible_probabilities(rbm_w, hidden_state);
+visible_state = sample_bernoulli(visible_probability);  %convert probability to binary state(>0.5)
+hidden_probability2 = visible_state_to_hidden_probabilities(rbm_w, visible_state);
+%hidden_state2 = sample_bernoulli(hidden_probability2);  %convert probability to binary state(>0.5)
+
+% 4 obtain the gradient .
+%  d_G_by_rbm_w1 = configuration_goodness_gradient(visible_state,
+%  hidden_state2);  % Q7
+
+d_G_by_rbm_w1 = configuration_goodness_gradient(visible_state, hidden_probability2);
+ret = d_G_by_rbm_w0 -d_G_by_rbm_w1;
+%error('not yet implemented');
 end
--- lecture13/a4/hidden_state_to_visible_probabilities.m
+++ lecture13/a4/hidden_state_to_visible_probabilities.m
@@ -3,5 +3,8 @@ function visible_probability = hidden_state_to_visible_probabilities(rbm_w, hidd
 % <hidden_state> is a binary matrix of size <number of hidden units> by <number of configurations that we're handling in parallel>.
 % The returned value is a matrix of size <number of visible units> by <number of configurations that we're handling in parallel>.
 % This takes in the (binary) states of the hidden units, and returns the activation probabilities of the visible units, conditional on those states.
-    error('not yet implemented');
+
+%according rbm weight and hidden unit state,calculate visible unit state.
+visible_probability = 1./(1+exp(-1*rbm_w'*hidden_state));  %rbm_w --- L2xL1   hidden--- L2xC  visi --- L1xC
+%error('not yet implemented');
 end
